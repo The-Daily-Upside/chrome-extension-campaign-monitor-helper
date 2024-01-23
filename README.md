1408b47998a9f1aa1d754d3e9614b1eb845a5156
Arash <arashsafari.dev@gmail.com>
2024-01-23 22:33:08 +0300

    Init version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# chrome-extension-campaign-monitor-helper
+# Chrome Extension Campaign Monitor Helper
+
 Chrome Extension for disabling "Inline CSS" when importing HTML email into campaign monitor
+
+## Local Setup
+
+Run:
+
+```
+~% npm install
+~% npm start
+
+```
+
+## Install the extension on Chrome/Brave
+
+-   Enter `chrome://extensions/` in Chrome/Brave URL
+-   Activate developer mode
+-   Load unpacked then choose the repo
+-   Ready!
+-   You can pin the extension for ease of use
---
+++ content.js
@@ -0,0 +1,58 @@
+/**
+ * This file is only loaded if the URL satisfies the matched pattern.
+ */
+
+/**
+ * DOMContentLoaded listener won't work because the page is already loaded
+ * CM dynamically pulls in content, so we need to wait for the checkbox to be added
+ */
+
+const checkCheckboxStatus = () => {
+    const checkbox = document.getElementById('move-css-inline-choice');
+    if (checkbox) {
+        if (checkbox.checked) {
+            /**
+             * Click works better than changing the checked property.
+             * CM will recheck after clicking on the import button.
+             */
+            checkbox.click();
+            // checkbox.checked = false;
+            // checkbox.dispatchEvent(new Event('change'));
+
+            /**
+             * Deactivate the checkbox so it can't be clicked again.
+             */
+
+            const checkboxParent = document.querySelector(
+                '.qa-checkbox-move-css-inline-choice'
+            );
+
+            if (checkboxParent) {
+                // Add a class to the parent element to style the checkbox
+                checkboxParent.style.pointerEvents = 'none';
+                checkboxParent.style.opacity = '0.3';
+            }
+        }
+    }
+};
+
+/**
+ * Check checkbox status when the page initially loads.
+ */
+checkCheckboxStatus();
+
+/**
+ * Use MutationObserver to detect changes in the DOM.
+ */
+const observer = new MutationObserver(() => {
+    // Check the checkbox status when the DOM changes
+    checkCheckboxStatus();
+});
+
+/**
+ * Observe changes in the document.
+ */
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
